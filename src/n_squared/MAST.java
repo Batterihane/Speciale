a29261f8b17fd1edfbf14fc84ec917216eaf4597
ThomasLange <thomashlange@gmail.com>
2016-02-09 16:36:11 +0100

    n_squared MAST maybe done, only size

--- src/n_squared/MAST.java
+++ src/n_squared/MAST.java
@@ -13,37 +13,57 @@ public class MAST {
     }
 
     public int getMAST(Phylogeny tree1, Phylogeny tree2){
-        int[] tree1TraversalNumbers = new int[tree1.getNodeCount()];
-        int[] tree2TraversalNumbers = new int[tree2.getNodeCount()];
+        int[] traversalNumbers = new int[tree1.getNodeCount() + tree2.getNodeCount()];
 
         int[][] subtreeMASTs = new int[tree1.getNodeCount()][tree2.getNodeCount()];
 
         PhylogenyNodeIterator tree1Iterator = tree1.iteratorPostorder();
-        PhylogenyNodeIterator tree2Iterator = tree1.iteratorPostorder();
 
         for (int i = 0 ; tree1Iterator.hasNext() ; i++){
             PhylogenyNode currentTree1Node = tree1Iterator.next();
-            tree1TraversalNumbers[currentTree1Node.getId()] = i;
+            traversalNumbers[currentTree1Node.getId()] = i;
+            PhylogenyNodeIterator tree2Iterator = tree2.iteratorPostorder();
             for (int j = 0 ; tree2Iterator.hasNext() ; j++){
                 PhylogenyNode currentTree2Node = tree2Iterator.next();
-                tree2TraversalNumbers[currentTree2Node.getId()] = j;
-                if(currentTree1Node.getNumberOfDescendants() == 0){
-                    if(currentTree2Node.getNumberOfDescendants() == 0){
+                traversalNumbers[currentTree2Node.getId()] = j;
+                if(currentTree1Node.isExternal()){
+                    if(currentTree2Node.isExternal()){
                         subtreeMASTs[i][j] = currentTree1Node.getName().equals(currentTree2Node.getName())? 1 : 0;
                     }
                     else{
-                        int child1TraversalNumber = tree2TraversalNumbers[currentTree2Node.getChildNode1().getId()];
-                        int child2TraversalNumber = tree2TraversalNumbers[currentTree2Node.getChildNode2().getId()];
+                        int child1TraversalNumber = traversalNumbers[currentTree2Node.getChildNode1().getId()];
+                        int child2TraversalNumber = traversalNumbers[currentTree2Node.getChildNode2().getId()];
                         subtreeMASTs[i][j] = Math.max(subtreeMASTs[i][child1TraversalNumber], subtreeMASTs[i][child2TraversalNumber]);
                     }
                 }
                 else{
+                    int tree1Child1TraversalNumber = traversalNumbers[currentTree1Node.getChildNode1().getId()];
+                    int tree1Child2TraversalNumber = traversalNumbers[currentTree1Node.getChildNode2().getId()];
 
+                    if(currentTree2Node.isExternal()){
+                        subtreeMASTs[i][j] = Math.max(subtreeMASTs[tree1Child1TraversalNumber][j], subtreeMASTs[tree1Child2TraversalNumber][j]);
+                    }
+                    else{
+                        int tree2Child1TraversalNumber = traversalNumbers[currentTree2Node.getChildNode1().getId()];
+                        int tree2Child2TraversalNumber = traversalNumbers[currentTree2Node.getChildNode2().getId()];
+
+                        subtreeMASTs[i][j] = max(
+                                subtreeMASTs[tree1Child1TraversalNumber][tree2Child1TraversalNumber] + subtreeMASTs[tree1Child2TraversalNumber][tree2Child2TraversalNumber],
+                                subtreeMASTs[tree1Child1TraversalNumber][tree2Child2TraversalNumber] + subtreeMASTs[tree1Child2TraversalNumber][tree2Child1TraversalNumber],
+                                subtreeMASTs[i][tree2Child1TraversalNumber],
+                                subtreeMASTs[i][tree2Child2TraversalNumber],
+                                subtreeMASTs[tree1Child1TraversalNumber][j],
+                                subtreeMASTs[tree1Child2TraversalNumber][j]
+                        );
+                    }
                 }
             }
         }
-        return 0;
+        return subtreeMASTs[tree1.getNodeCount()-1][tree2.getNodeCount()-1];
     }
 
+    public int max(int a, int b, int c, int d, int e, int f){
+        return Math.max(a, Math.max(b, Math.max(c, Math.max(d, Math.max(e, f)))));
+    }
 
 }
--- src/n_squared/Runner.java
+++ src/n_squared/Runner.java
@@ -1,7 +1,23 @@
 package n_squared;
 
+import Utilities.ForesterNewickParser;
+import org.forester.phylogeny.Phylogeny;
+import org.forester.phylogeny.iterators.PhylogenyNodeIterator;
+
 public class Runner {
     public static void main(String[] args) {
+        ForesterNewickParser foresterNewickParser = new ForesterNewickParser();
 
+        Phylogeny tree1 = foresterNewickParser.parseNewickFile("trees\\Tree1.new");
+        Phylogeny tree2 = foresterNewickParser.parseNewickFile("trees\\Tree2.new");
+        //foresterNewickParser.displayPhylogeny(tree);
+        //System.out.println(tree.getRoot().getNumberOfDescendants());
+
+
+        PhylogenyNodeIterator tree1Iterator = tree1.iteratorPostorder();
+        PhylogenyNodeIterator tree2Iterator = tree2.iteratorPostorder();
+
+        MAST mast = new MAST();
+        System.out.println(mast.getMAST(tree1, tree2));
     }
 }
