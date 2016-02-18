e3d523c42b910b081ec35f9fa6e4dade6ca89916
ThomasLange <thomashlange@gmail.com>
2016-02-18 16:11:58 +0100

    add nlogn MAST decomposition

---
+++ src/nlogn/MAST.java
@@ -0,0 +1,66 @@
+package nlogn;
+
+import org.forester.phylogeny.Phylogeny;
+import org.forester.phylogeny.PhylogenyNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by Thomas on 18-02-2016.
+ */
+public class MAST {
+
+    private List<PhylogenyNode> computeFirstDecomposition(Phylogeny tree){
+        List<PhylogenyNode> result = new ArrayList<>();
+
+        PhylogenyNode currentNode = tree.getRoot();
+        while (!currentNode.isExternal()){
+            result.add(currentNode);
+            PhylogenyNode firstChild = currentNode.getChildNode1();
+            PhylogenyNode secondChild = currentNode.getChildNode2();
+            currentNode = firstChild.getNumberOfExternalNodes() > secondChild.getNumberOfExternalNodes()
+                    ? firstChild : secondChild;
+        }
+        result.add(currentNode);
+
+        return result;
+    }
+
+    private List<List<PhylogenyNode>> computeSecondDecomposition(Phylogeny tree){
+        List<List<PhylogenyNode>> result = new ArrayList<>();
+        Stack<PhylogenyNode> remainingStartNodes = new Stack<>();
+        PhylogenyNode root = tree.getRoot();
+        if(root.isExternal()) return result;
+        remainingStartNodes.add(root);
+
+        while (!remainingStartNodes.isEmpty()){
+            PhylogenyNode firstNode = remainingStartNodes.pop();
+            List<PhylogenyNode> newPath = new ArrayList<>();
+            PhylogenyNode currentNode = firstNode;
+
+            while (!currentNode.isExternal()){
+                newPath.add(currentNode);
+                currentNode.setLink(firstNode);
+                PhylogenyNode firstChild = currentNode.getChildNode1();
+                PhylogenyNode secondChild = currentNode.getChildNode2();
+                if(firstChild.getNumberOfExternalNodes() > secondChild.getNumberOfExternalNodes()){
+                    currentNode = firstChild;
+                    if(!secondChild.isExternal()) remainingStartNodes.push(secondChild);
+                }
+                else {
+                    currentNode = secondChild;
+                    if(!firstChild.isExternal()) remainingStartNodes.push(firstChild);
+                }
+            }
+            newPath.add(currentNode);
+            currentNode.setLink(firstNode);
+            result.add(newPath);
+        }
+
+        return result;
+    }
+
+}
